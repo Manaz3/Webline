877ab632c6f8246df321a10282b30098e84f5422
Manaz3 <max_nazarow@mail.ru>
2024-05-17 21:10:13 +0300

    jest

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,7 +3,12 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ['standard-with-typescript', 'plugin:react/recommended'],
+  extends: [
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'prettier',
+    'plugin:@typescript-eslint/recommended',
+  ],
   overrides: [
     {
       env: {
@@ -21,6 +26,8 @@ module.exports = {
   },
   plugins: ['react', '@typescript-eslint'],
   rules: {
-    'react/jsx-indent': [2, { indentMode: 4, ignoreTernaryOperator: true }],
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": ["error"]
+
   },
 };
